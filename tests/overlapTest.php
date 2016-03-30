9126ee527508f03a2e06888f5c2d354feb4c66fa
Seth Battis <sethbattis@stmarksschool.org>
2016-03-30 12:12:09 -0400

    phpunit

---
+++ tests/overlapTest.php
@@ -0,0 +1,47 @@
+<?php
+	
+use Battis\DataUtilities;
+	
+class OverlapTest extends PHPUnit_Framework_TestCase {
+
+	public function testOverlapLeftRight() {
+		$a = 'abcdef';
+		$b = 'defghi';
+		
+		// expected use case
+		$this->assertEquals('def', DataUtilities::overlap($a, $b));
+	}
+
+	public function testOverlapRightLeft() {
+		$a = 'abcdef';
+		$b = 'defghi';
+		
+		// reversed use case
+		$this->assertEquals('def', DataUtilities::overlap($b, $a));
+	}
+
+	public function testOverlapRightLeftWithoutSwap() {
+		$a = 'abcdef';
+		$b = 'defghi';
+
+		// reversed use case, no swapping
+		$this->assertEquals('', DataUtilities::overlap($b, $a, false));
+	}
+
+	public function testNoOverlap() {
+		$a = 'abcdef';
+		$c = 'lmnop';
+		$d = new DateTime();
+		
+		// no overlap present
+		$this->assertEquals('', DataUtilities::overlap($a, $c));
+	}
+
+	public function testNonstring() {
+		$a = 'abcdef';
+		$d = new DateTime();
+		
+		// non-string argument
+		$this->assertEquals('', DataUtilities::overlap($a, $d));
+	}
+}
